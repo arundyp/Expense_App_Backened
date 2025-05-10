651e51432cc163af0034ad08d9bd9baded5ad8bc
arundyp <arundyp@gmail.com>
2025-05-10 10:33:25 -0700

    Initial commit

---
+++ ExpenseApplication/src/test/java/com/arun/ExpenseApplication/ExpenseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arun.ExpenseApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
